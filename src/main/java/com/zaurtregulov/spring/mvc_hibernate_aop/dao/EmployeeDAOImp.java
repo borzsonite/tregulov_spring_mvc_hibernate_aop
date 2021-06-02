95a400269163cff9d4668eaf9eaa252374a124d2
Sergey Rozhkov <borzsonite@gmail.com>
2021-06-02 12:59:48 +0300

    70/14.00

---
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/dao/EmployeeDAOImp.java
@@ -0,0 +1,12 @@
+package com.zaurtregulov.spring.mvc_hibernate_aop.dao;
+
+import com.zaurtregulov.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+public class EmployeeDAOImp implements EmployeeDAO {
+    @Override
+    public List<Employee> getAllEmployees() {
+        return null;
+    }
+}
