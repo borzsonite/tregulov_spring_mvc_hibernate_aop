bda682941a0bc911dcda347a439cc725b89aed23
Sergey Rozhkov <borzsonite@gmail.com>
2021-06-02 17:06:58 +0300

    70 17.00

--- src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/dao/EmployeeDAOImp.java
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/dao/EmployeeDAOImp.java
@@ -1,12 +1,27 @@
 package com.zaurtregulov.spring.mvc_hibernate_aop.dao;
 
 import com.zaurtregulov.spring.mvc_hibernate_aop.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
+@Repository
 public class EmployeeDAOImp implements EmployeeDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
     @Override
+    @Transactional
     public List<Employee> getAllEmployees() {
-        return null;
+
+        Session session = sessionFactory.getCurrentSession();
+        List<Employee> allEmployees = session.createQuery("from Employee", Employee.class)
+                .getResultList();
+        return allEmployees;
     }
 }
